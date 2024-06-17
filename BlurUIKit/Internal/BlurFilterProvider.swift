fb781ef9f015ee0568b550ab4f9a502e19c81d2a
Tim Oliver <git@tim.dev>
2024-06-17 21:46:17 +0900

    Implemented initial variable blur logic

--- BlurUIKit/Internal/BlurFilterProvider.swift
+++ BlurUIKit/Internal/BlurFilterProvider.swift
@@ -7,28 +7,42 @@
 
 import UIKit
 
-protocol BlurFilterProvivadable: NSObjectProtocol {
-    func filter(withName: String) -> NSObject
-}
-
 // An internal class that creates, manages and vends the blur filters
 // used by the public classes of this framework.
 internal class BlurFilterProvider {
 
-    // A shared blur visual effect view is used to hook and extract a reference
+    // A shared blur visual effect view that is used to hook and extract a reference to the CAFilter class
     static let blurView = UIVisualEffectView(effect: UIBlurEffect(style: .regular))
 
-    // Vends a filter
-    static func blurFilter(named: String) -> NSObject? {
-        // Fetch the filter from out of the shared blur view
-        guard let backdropView = blurView.subviews.first(where: {
-            String(describing: $0).lowercased().contains("backdrop")
-        }), let filter = backdropView.layer.filters?.first as? NSObject else {
+    /// Vends a newly instantiated CAFilter instance with the provided filter name
+    /// - Parameter name: The name of the filter that this filter should be instantiated with
+    /// - Returns: The new CAFilter object, or nil if the filter if it couldn't be created.
+    static func blurFilter(named name: String) -> NSObject? {
+        // The only private method we need is 'CAFilter.filterWithType:'
+        // We probably don't need to obfuscate this since it's not prefixed with a '_'
+        let selectorName = ["Type:", "With", "filter"].reversed().joined()
+        let selector = NSSelectorFromString(selectorName)
+
+        // Fetch a filter from out of the shared blur view so we can access the CAFilter class.
+        guard let backdropView = findSubview(in: blurView, containing: "backdrop"),
+              let filter = backdropView.layer.filters?.first as? NSObject else {
             return nil
         }
 
-
+        // Using the blur view filter object as a base, fetch the CAFilter class, and use that to instantiate a new object
+        let type = type(of: filter)
+        if type.responds(to: selector) {
+           return type.perform(selector, with: name).takeUnretainedValue() as? NSObject
+        }
 
         return nil
+    }
+
+    /// Loop through a view's subviews and find the one with its class name containing the provided string
+    /// - Parameter view: The view whose subviews will be searched.
+    /// - Parameter name: The portion of the name of the subiew to find.
+    /// - Returns: The subview if it is found, or nil otherwise.
+    static func findSubview(in view: UIView, containing name: String) -> UIView? {
+        view.subviews.first { String(describing: $0).lowercased().contains(name.lowercased()) }
     }
 }
--- BlurUIKit/VariableBlurView.swift
+++ BlurUIKit/VariableBlurView.swift
@@ -13,6 +13,23 @@ import UIKit
 /// without any hard border lines.
 public class VariableBlurView: UIVisualEffectView {
 
+    // The maximum blur radius of the blur view when its gradient is at full opacity
+    public var maximumBlurRadius = 2.0 {
+        didSet { updateBlurFilter() }
+    }
+
+    // Performs an update when the frame changes
+    public override var frame: CGRect {
+        didSet {
+            if frame.size != oldValue.size {
+                updateBlurFilter()
+            }
+        }
+    }
+
+    // The variable blur view filter
+    private let variableBlurFilter = BlurFilterProvider.blurFilter(named: "variableBlur")
+
     // MARK: - Initialization
 
     init() {
@@ -38,7 +55,50 @@ public class VariableBlurView: UIVisualEffectView {
     // MARK: - View Lifecycle
 
     public override func didMoveToSuperview() {
-        let filter = BlurFilterProvider.blurFilter(named: "Wee")
+        super.didMoveToSuperview()
+        configureView()
+        updateBlurFilter()
     }
 
+    public override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        super.traitCollectionDidChange(previousTraitCollection)
+        configureView()
+        updateBlurFilter()
+    }
+
+    // MARK: - Private
+
+    // One-time setup logic
+    private func configureView() {
+        guard superview != nil, let variableBlurFilter else { return }
+
+        // Find the overlay view (The one that lightens or darkens these blur views) and hide it.
+        BlurFilterProvider.findSubview(in: self, containing: "subview")?.isHidden = true
+
+        // Find the backdrop view (The one that repeats the content drawn behind it) and apply the blur filter.
+        if let backdropView = BlurFilterProvider.findSubview(in: self, containing: "backdrop") {
+            backdropView.layer.filters = [variableBlurFilter]
+            backdropView.layer.setValue(0.5, forKey: "scale")
+        }
+    }
+
+    // Update the parameters of the blur filter
+    private func updateBlurFilter() {
+        variableBlurFilter?.setValue(gradientMaskImage(), forKey: "inputMaskImage")
+        variableBlurFilter?.setValue(maximumBlurRadius, forKey: "inputRadius")
+        variableBlurFilter?.setValue(true, forKey: "inputNormalizeEdges")
+    }
+
+    // Generates a gradient bitmap to be used with the blur filter
+    private func gradientMaskImage() -> CGImage? {
+        let size = CGSize(width: 1, height: 5)
+        let graphicsRenderer = UIGraphicsImageRenderer(size: size)
+        let image = graphicsRenderer.image { context in
+            let gradientColor = UIColor(red: 0.000, green: 0.000, blue: 0.000, alpha: 1.00)
+            let gradientColor2 = UIColor(red: 0.000, green: 0.000, blue: 0.000, alpha: 0.000)
+            let gradient = CGGradient(colorsSpace: nil, colors: [gradientColor.cgColor, gradientColor2.cgColor] as CFArray, locations: [0, 1])!
+            context.cgContext.drawLinearGradient(gradient, start: CGPoint(x: 0.5, y: 00), end: CGPoint(x: 0.5, y: 5), options: [])
+        }
+        return image.cgImage
+    }
 }
