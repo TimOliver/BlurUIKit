f8ea8ad2598682ebd57f05cf89e49ef44910e0e5
Tim Oliver <git@tim.dev>
2025-01-01 14:07:17 +0800

    Happy New Year! 🥳

--- BlurUIKit/Internal/BlurFilterProvider.swift
+++ BlurUIKit/Internal/BlurFilterProvider.swift
@@ -1,6 +1,6 @@
 //
 //  BlurFilterProvider.swift
-//  Copyright (c) 2024 Tim Oliver
+//  Copyright (c) 2024-2025 Tim Oliver
 //
 //  Permission is hereby granted, free of charge, to any person obtaining a copy
 //  of this software and associated documentation files (the "Software"), to deal
