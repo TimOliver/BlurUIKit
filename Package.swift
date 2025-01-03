7bad7081fd8f92daab41c1feadba7b7a70e3f6ba
Simon Jarbrant <simonjarbrant@me.com>
2025-01-03 17:37:46 +0100

    Fix Package.swift

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.3
 import PackageDescription
 
 let package = Package(
@@ -13,7 +13,7 @@ let package = Package(
     targets: [
         .target(
             name: "BlurUIKit",
-            path: "BlurUIKit/",
+            path: "BlurUIKit/"
         )
     ]
 )
